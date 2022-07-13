b885ddde43b2da6facc0e148f14664383ceabdb5
Eva Laurent <103074596+Evajuliab@users.noreply.github.com>
2022-07-13 14:23:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const arr = [48, -2, 6, 12, 0, -4]
 
-//Exercice merge
-
+//Exercice merge 
+// Tuto trouvé sur internet
 
 function merge(left, right) {
-    let sortedArr = []; 
-  
+    let sortedArr = [] 
+
     while (left.length && right.length) {
-      // insertion du plus petit element dans le sortedArr
+      // Insertion du plus petit élèment dans le sortedarr
       if (left[0] < right[0]) {
-        sortedArr.push(left.shift());
+        sortedArr.push(left.shift())
       } else {
-        sortedArr.push(right.shift());
+        sortedArr.push(right.shift())
       }
     }
-
+    // Use spread operators to create a new array, combining the three arrays
+    return [...sortedArr, ...left, ...right]
   }
 
 
   function mergeSort(arr) {
-    const half = arr.length / 2;
-  
-    // the base case is array length <=1
-    if (arr.length <= 1) {
-      return arr;
-    }
-  
-    const left = arr.splice(0, half); // the first half of the array
-    const right = arr;
-    return (mergeSort(left), mergeSort(right));
+    //Cas de base
+    if (arr.length <= 1) return arr
+
+    let mid = Math.floor(arr.length / 2)
+    
+    // recursivité
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
   }
 
- console.log(merge(arr))
+  console.log(mergeSort(arr))
